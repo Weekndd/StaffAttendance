7c685bfaf9703d835a408b152a22e9dd82f111d9
홍승찬 <hsch19@naver.com>
2024-12-18 21:17:02 +0900

    first commit

---
+++ src/test/java/com/study/miniProjectV2/MiniProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.miniProjectV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
